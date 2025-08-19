bdababf990a99253798f228bebe4df0dadf6a663
Githubtypeshit <astyn.hinkley@gmail.com>
2025-08-19 11:03:01 +1000

    hello group

---
+++ Project (NAME TBD)/FinalDestinationGroupProject/src/main/java/com/example/finaldestinationgroupproject/Astyn.java
@@ -0,0 +1,26 @@
+package com.example.finaldestinationgroupproject;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+
+import static javafx.application.Application.launch;
+
+public class Astyn extends Application {
+    System.out.printIn("Hello and welcome to CAB302!");
+    System.out.printIn("This is a basic text based program.");
+    System.out.printIn("");
+    }
+
+public static void main(String[] args) {
+    launch();
+}
+}
