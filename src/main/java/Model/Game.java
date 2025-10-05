9cc3cfe524b57a0625877ccd209301ab1df08689
Bella11550872 <n11550872@qut.edu.au>
2025-10-05 13:19:09 +1000

    - javadoc comments for Game methods

--- src/main/java/Model/Game.java
+++ src/main/java/Model/Game.java
@@ -6,8 +6,16 @@ public class Game {
     private int gameID;
     private String gameName;
     private File bucketFile;
+    private File wordFile;
     private Bucket[] buckets = {};
 
+    /**
+     * Constructor for the Game class
+     * @param gameId int identifying number for the game
+     * @param gameName string name of the game
+     * @param bucketFile txt file with all the bucket information for the whole system, each line will follow the format 'BucketID-BucketName-GameID<<bucketImagePath>>'
+     * @param wordFile txt file with all the words used by the whole system, each line will follow the format 'WordID-Word-BucketID'
+     */
     public Game(int gameId, String gameName, File bucketFile, File wordFile) {
         this.gameID = gameId;
         this.bucketFile = bucketFile;
@@ -27,14 +35,43 @@ public class Game {
         return null; //dummy value
     }
 
-    public void createBuckets(){
+
+    /**
+     * Called by the constructor:
+     * Will go through each line in the bucketFile and create all the buckets that belong to this game (have matching gameIDs).
+     */
+    private void createBuckets(){
         //logic
         //for each line in text file get line:
 
 
     }
 
-    //given a string from the BucketIDtect file it will return the BucketID
+    /**
+     * Method will create a Bucket object for a given line in the bucketFile txt
+     * @param textFileLine text file line from the bucketFile file, as a string
+     * @return instance of the Bucket object from the text file line
+     */
+    public Bucket createBucket(String textFileLine) {
+        int bucketID = extractBucketID(textFileLine);
+        String bucketName = extractBucketName(textFileLine);
+        String bucketImagepath = extractBucketImagePath(textFileLine);
+
+        Bucket bucket = new Bucket(bucketID, bucketName, bucketImagepath);
+        return bucket;
+    }
+
+    // returns the position of the next '-' in the line from the starting index given
+    public int positionOfNextBreak(int startPosition, String textFileLine){
+        final String breakCharacter = "-";
+        int nextBreak = textFileLine.indexOf(breakCharacter,startPosition);
+        return nextBreak;
+    }
+    /**
+     * Given a string from the bucketFile it will return the BucketID
+     * @param textFileLine a line from the bucketFile File as a string, which will have the format 'BucketID-BucketName-GameID<<bucketImagePath>>'
+     * @return int value of 'BucketID' from the text file line
+     */
     public int extractBucketID(String textFileLine) {
         //get the position of the next break
         int nextBreakPosition = positionOfNextBreak(0,textFileLine);
@@ -51,13 +88,11 @@ public class Game {
         return bucketId_int;//dummy value
     }
 
-    // returns the position of the next '-' in the line from the starting index given
-    public int positionOfNextBreak(int startPosition, String textFileLine){
-        final String breakCharacter = "-";
-        int nextBreak = textFileLine.indexOf(breakCharacter,startPosition);
-        return nextBreak;
-    }
-
+    /**
+     * Given a string from the bucketFile it will return the BucketName
+     * @param textFileLine a line from the bucketFile File as a string, which will have the format 'BucketID-BucketName-GameID<<bucketImagePath>>'
+     * @return string value of 'BucketName' from the text file line
+     */
     public String extractBucketName(String textFileLine) {
         //get character position in string for the start of the Bucket name: the first '-' in the file
         // + 1 to it so that it doesn't include the first break character later
@@ -71,6 +106,11 @@ public class Game {
         return bucketNameSegment;//dummy value
     }
 
+    /**
+     * Given a string from the bucketFile it will return the BucketImagePath
+     * @param textFileLine a line from the bucketFile File as a string, which will have the format 'BucketID-BucketName-GameID<<bucketImagePath>>'
+     * @return string value of 'BucketImagePath' from the text file line
+     */
     public String extractBucketImagePath(String textFileLine) {
         //define what the starting character is
         final String startCharacter = "<<";
@@ -87,18 +127,16 @@ public class Game {
         return bucketPathSegment;
     }
 
-    public Bucket createBucket(String textFileLine) {
-        int bucketID = extractBucketID(textFileLine);
-        String bucketName = extractBucketName(textFileLine);
-        String bucketImagepath = extractBucketImagePath(textFileLine);
-
-        Bucket bucket = new Bucket(bucketID, bucketName, bucketImagepath);
-        return bucket;
-    }
-
+    /**
+     * Given a string from the bucketFile it will return the GameID
+     * @param textFileLine a line from the bucketFile File as a string, which will have the format 'BucketID-BucketName-GameID<<bucketImagePath>>'
+     * @return int value of 'GameID' from the text file line
+     */
     public int extractGameID(String textFileLine) {
         return 0; //dummy value
     }
+
+
 
     public String getGameName() {
         return "";//dummy value
