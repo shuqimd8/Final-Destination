4e74b98566a226e8b71ecaf6a35471c42e58d57b
Jy905 <131685393+Jy905@users.noreply.github.com>
2025-10-25 15:18:24 +1000

    upload of Javadoc

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.learneria.controller","l":"com.learneria"},{"m":"com.learneria.controller","l":"com.learneria.controller"},{"m":"com.learneria.controller","l":"com.learneria.utils"}];updateSearchResults();
